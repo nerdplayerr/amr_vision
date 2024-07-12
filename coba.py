d1e10b970f05f497bafd23d1600175eb0d61d908
nerdplayerr <hafizalifian14@gmail.com>
2024-07-12 14:43:10 +0700

    new nav

--- coba.py
+++ coba.py
@@ -1,11 +1,13 @@
 import cv2
 import time
+import numpy as np
 from ultralytics import YOLO
 import serial
 import data_parser
+import mqtt_publisher
 
 # Load YOLOv8 model
-model = YOLO('best.pt', task='detect')
+model = YOLO('new.engine', task='detect')
 print(model.names)
 
 # Initialize web camera
@@ -16,7 +18,8 @@ webcamera.set(4,480)
 # Parameters for distance estimation
 KNOWN_WIDTH = 35
 KNOWN_DIST = 90
-FOCAL_LENGTH = (600, 800)
+FOCAL_LENGTH = (600, 630)
+conv_factor = 0.1419
 # KNOWN_HEIGHT = 3  # meters (example: height of a person)
 # FOCAL_LENGTH = 2000 # example value in pixels (calibrate your camera to get this value)
 
@@ -26,16 +29,19 @@ new_frame_time = 0
 
 # Serial Mode
 
-# serial_port = '/dev/ttyUSB0'  # or 'COM1' for Windows
-# baud_rate = 115200
+serial_port = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0' 
+baud_rate = 115200
 
-# # Create a serial object
-# ser = serial.Serial(serial_port,
-#                      baudrate=baud_rate,
-#                      timeout=5.0,
-#                      bytesize=8,
-#                      parity='N',
-#                      stopbits=1)
+connect_web = mqtt_publisher.connect()
+
+id_web = 1
+# Create a serial object
+ser = serial.Serial(serial_port,
+                     baudrate=baud_rate,
+                     timeout=5.0,
+                     bytesize=8,
+                     parity='N',
+                     stopbits=1)
 
 def fl_cal(width_pixel, known_distance, known_width):
     return (width_pixel * known_distance)/known_width
@@ -43,6 +49,9 @@ def fl_cal(width_pixel, known_distance, known_width):
 def estimate_distance(known_width, width_pixel, focal_length):
     """Estimate the distance from the camera to the object."""
     return abs(known_width * focal_length) / width_pixel
+
+def euclidean_distance(point1, point2):
+    return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
 while True:
     ret, frame = webcamera.read()
@@ -65,6 +74,9 @@ while True:
     # Draw FPS on the frame
     cv2.putText(frame, fps, (7, 20), font, 1, (100, 255, 0), 1, cv2.LINE_AA)
     
+    # Create centroid list of objects
+    centroids = []
+
     for r in results :
         # Get the boxes and track IDs
         boxes = r.boxes
@@ -84,8 +96,14 @@ while True:
             w_pix = x2 - x1
             perceived_height = y2 - y1
             object_center = (x1 + x2) / 2
+
+            
+            centroid = ((x1 + x2) / 2, (y1 + y2) / 2)
+            centroids.append(centroid)
             # focal = fl_cal(w_pix, KNOWN_DIST, KNOWN_WIDTH)
             # print(focal)
+            
+            
             if classes == 0:
                 distance = estimate_distance(KNOWN_WIDTH, w_pix, FOCAL_LENGTH[0])
                 # print(distance)
@@ -97,32 +115,73 @@ while True:
                 direction = 'right'
             else: direction = 'left'
 
-            if 0 < distance < 50 and direction == 'right':
+            min_dist_0 = 50
+            min_dist_1 = 80
+            min_gap = 450          # +-50 cm
+
+            if classes == 0 and distance < min_dist_0 and direction == 'right':
                 print("---STOP---")
-                # data_parser.send_command(ser,0,0,0) # kanan-kiri, maju-mundur, rotation
+                data_parser.send_command(ser,0,0,0,0) # kanan-kiri, maju-mundur, rotation
+                
                 for i in range(5):
                     print("STOP", i + 1)
                     time.sleep(1)
 
-                # print("------Kanan")
-                # data_parser.send_command(ser,200,0,0) # kanan-kiri, maju-mundur, rotation
+                print("------Kanan")
+                data_parser.send_command(ser,id_web, 200,0,0) # kanan-kiri, maju-mundur, rotation
+                id_web += 1
 
-            elif 0 < distance < 50 and direction == 'left':
+            elif classes == 0 and distance <= min_dist_0 and direction == 'left':
                 print("---STOP---")
-                # data_parser.send_command(ser,0,0,0) # kanan-kiri, maju-mundur, rotation
+                data_parser.send_command(ser,0,0,0,0) # kanan-kiri, maju-mundur, rotation
                 for i in range(5):
                     print("STOP", i + 1)
                     time.sleep(1)
                 print("------Kiri")
-                # data_parser.send_command(ser,-200,0,0) # kanan-kiri, maju-mundur, rotation
-            else:
-                print("---CLEAR---")
-                # data_parser.send_command(ser,0,200,0) # kanan-kiri, maju-mundur, rotation
+                data_parser.send_command(ser,id_web,-200,0,0) # kanan-kiri, maju-mundur, rotation
+                id_web += 1
+
+            elif classes == 1 and distance <= min_dist_1:
+                print("---STOP---")
+                data_parser.send_command(ser,0,0,0,0) # kanan-kiri, maju-mundur, rotation
+                for i in range(5):
+                    print("STOP", i + 1)
+                    time.sleep(1)
+                
+        
+            # else:
+                # print("---CLEAR---")
+                # data_parser.send_command(ser,id_web,200,0,0) # kanan-kiri, maju-mundur, rotation
+                # id_web += 1
+
+            if len(centroids) > 1 and distance < 150:
+                gap = np.full((len(centroids), len(centroids)), np.inf)
+                for i, point1 in enumerate(centroids):
+                    for j, point2 in enumerate(centroids):
+                        if i != j:
+                            gap[i][j] = euclidean_distance(point1, point2)
+
+                # Find the pair with the shortest distance
+                i, j = np.unravel_index(gap.argmin(), gap.shape)
+                shortest_gap = gap[i][j]
+                print(int(shortest_gap))
+
+
+                if int(shortest_gap) < min_gap and distance <= 75:
+                    print('---BLOCKED---')
+                    
+                    print("---STOP---")
+                    data_parser.send_command(ser,0,0,0,0) # kanan-kiri, maju-mundur, rotation
+                    status = 'A55A21041P00P03P01P03P02P03P03P03'
+                    mqtt_publisher.publish(connect_web, status)
+                
+                
 
     # Loop through detected objects
             cv2.putText(annotated_frame, f'Distance: {int(distance):.2f}cm', (int(x1), int(y1) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             cv2.putText(annotated_frame, f'Direction: {direction}', (int(x1), int(y2) + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    
+
+        # print(centroids)
     # Display the total count of detected objects
     cv2.putText(annotated_frame, f"Total: {len(results[0].boxes)}", (10, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
